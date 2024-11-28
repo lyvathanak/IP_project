0bfd19c09d6544622a634f9278ff1ed92e27e493
gitmeas02 <measreaksmey02@gmail.com>
2024-11-28 16:35:13 +0700

    make it better css header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
-    <Header></Header>
-  </template>
-  
-  <script>
-  import Header from './components/Header.vue';
-  
-  export default {
-    components: {
-      Header
-    }
-  };
-  </script>
-  
-  <style>
-  /* Add global styles if needed */
-  </style>
-  
+  <Header></Header>
+  <InputField/>
+</template>
+
+<script>
+import Header from '@/components/Header.vue';
+import InputField from '@/components/InputField.vue';
+
+export default {
+  components: {
+    Header,InputField
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+}
+</style>
---
+++ src/components/InputField.vue
@@ -0,0 +1,31 @@
+<template>
+ <input type="text" name="name"  placeholder="name">
+</template>
+<script>
+export default{}
+</script>
+<style>
+input{
+width: 313px;
+height: 43.4px;
+border:1px #dbdbdb solid;
+transition: all 0.3s ease;
+outline: none;
+padding-left: 17px;
+padding-top: 0px;
+padding-bottom: 0px;
+background-color: #FBFBFB;
+}
+input:focus{
+    border: 1px #dbdbdb solid;
+    border-radius: 9px;
+    /* box-shadow: 0 0 8px rgba(76, 175, 80, 0.5); */
+}
+input[type="text"] {
+  padding-left: 17px; /* Removes all padding inside the input */
+  font-size: 16px;
+  outline: none;
+  color:rgb(0, 0, 0);
+  border-radius: 9px;
+}
+</style>
