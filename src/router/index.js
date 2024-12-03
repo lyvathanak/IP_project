91eb94ee8d5968312903f8bacdb31c0541478cca
gitmeas02 <measreaksmey02@gmail.com>
2024-12-03 12:38:28 +0700

    Struture in main

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,39 @@
+import HomeView from '@/views/HomeView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-
-
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    
+    {
+      path:"/",
+      name:"home",
+      component:HomeView,
+    },
+    // {
+    //   path:"/products",
+    //   name:"products",
+    //   component:ProductView,
+    // },
+    // {
+    //   path:"/product/details",
+    //   name:"product Details",
+    //   component:DetailView,
+    // },
+    // {
+    //   path:"/lists",
+    //   name:"cart",
+    //   component:CartListView,
+    // },
+    // {
+    //   path:"/paymentMethod",
+    //   name:"paymentMethod",
+    //   component:PaymentView,
+    // },
+    // {
+    //   path:"/account",
+    //   name:"account",
+    //   component:AccountView,
+    // },
   ],
 })
 
