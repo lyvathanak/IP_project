4a073aa1693ed62803cd3898b5330a716d03b243
yuminahh <ngovxnak@gmail.com>
2024-12-04 10:49:45 +0700

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,27 @@
 <template>
-<Header/>
+    <div>
+        <Header/>
+        <main>
+            <div>
+                Body
+            </div>
+        </main>
+        <Footer/>
+    </div>
 </template>
+
 <script>
 import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
 export default{
     name: 'HomeView',
     components:{
-        Header
-    }
-}
+        Header,
+        Footer,
+    },
+};
 </script>
+
+<style scoped>
+
+</style>
