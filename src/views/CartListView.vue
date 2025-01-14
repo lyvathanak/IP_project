6bf5b6ac787f97130b636644e49ca019649c45e3
gitmeas02 <measreaksmey02@gmail.com>
2025-01-14 18:06:24 +0700

    delete CatTotal.vue

--- src/views/CartListView.vue
+++ src/views/CartListView.vue
@@ -1,18 +1,16 @@
 <template>
 <Header/>
 <ListAfterBuy/>
-<!-- <CartTotal/> -->
 <Footer/>
 </template>
 <script>
-import CartTotal from '@/components/CartTotal.vue';
 import Footer from '@/components/Footer.vue';
 import Header from '@/components/Header.vue';
 import ListAfterBuy from '@/components/listAfterBuy.vue';
 
 export default{
     components:{
-   Header,ListAfterBuy,CartTotal,Footer
+   Header,ListAfterBuy,Footer
     }
 }
 </script>
